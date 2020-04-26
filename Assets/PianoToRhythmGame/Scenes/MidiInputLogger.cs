339e14e8d7a073fceb86799c118c7a85f3944c5f
Toranosuke Ichikawa <fastrivertiger@gmail.com>
2020-04-26 18:35:27 +0900

    initial commit: make project, add Unity gitignore, add UniRx and MidiJack

---
+++ Assets/PianoToRhythmGame/Scenes/MidiInputLogger.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using MidiJack;
+
+namespace PianoToRhythmGame.Debugging
+{
+    public class MidiInputLogger : MonoBehaviour
+    {
+        const int NUM_NOTES = 128;
+
+        // Update is called once per frame
+        void Update()
+        {
+            for (var noteIndex = 0; noteIndex < NUM_NOTES; noteIndex++)
+            {
+                var onNote = MidiMaster.GetKeyDown(noteIndex);
+                if (onNote)
+                {
+                    Debug.Log($"On:{noteIndex}");
+                }
+            }
+        }
+    }
+}
