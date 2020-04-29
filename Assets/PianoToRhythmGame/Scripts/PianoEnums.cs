5d1a87b29b835650846b10b58b9c7ef0695d01ab
Toranosuke Ichikawa <fastrivertiger@gmail.com>
2020-04-29 23:06:34 +0900

    add enums

---
+++ Assets/PianoToRhythmGame/Scripts/PianoEnums.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace PianoToRhythmGame
+{
+    public enum PianoKey
+    {
+        // while tone
+        White,
+        // half tone
+        Black,
+    }
+
+    public enum PianoWhiteKey
+    {
+        Do,
+        Re,
+        Mi,
+        Fa,
+        Sol,
+        La,
+        Si,
+    }
+}
