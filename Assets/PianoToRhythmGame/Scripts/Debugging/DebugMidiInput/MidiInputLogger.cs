14af78b3ae4cc789149d10df97fb460659d0f8e7
Toranosuke Ichikawa <fastrivertiger@gmail.com>
2020-05-01 07:53:10 +0900

    add TMPro log

---
+++ Assets/PianoToRhythmGame/Scripts/Debugging/DebugMidiInput/MidiInputLogger.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using MidiJack;
+using TMPro;
+
+namespace PianoToRhythmGame.Debugging
+{
+    public class MidiInputLogger : MonoBehaviour
+    {
+        [SerializeField]
+        int numTotalNotes = 128;
+
+        [SerializeField]
+        TextMeshProUGUI _logText;
+
+
+        // Update is called once per frame
+        void Update()
+        {
+            string log = "";
+            for (var noteIndex = 0; noteIndex < numTotalNotes; noteIndex++)
+            {
+                var velocity = MidiMaster.GetKey(noteIndex);
+                if (velocity > 0f)
+                {
+                    log += $"Note({noteIndex}) Velocity: {velocity}\n";
+                }
+            }
+
+            DisplayLog(log);
+        }
+
+        void DisplayLog(string message)
+        {
+            Debug.Log(message);
+
+            if (_logText != null)
+            {
+                _logText.text = message;
+            }
+        }
+    }
+}
